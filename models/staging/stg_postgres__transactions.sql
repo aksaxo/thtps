b669af074f2ecbd12139e9bf16b992c3aeadbe6f
Akshat Saxena <saxenaakshat16@gmail.com>
2025-07-14 15:47:04 +0000

    stages source models

---
+++ models/staging/stg_postgres__transactions.sql
@@ -0,0 +1,22 @@
+with
+
+source as (
+    select *
+    from {{ source('public', 'transactions') }}
+)
+
+, final as (
+    select
+        -- ids
+        id as transaction_id
+        , customer_id
+        , product_id
+        -- numerics
+        , quantity
+        -- dates
+        , transaction_date
+    from source
+)
+
+select *
+from final
