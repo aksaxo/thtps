1054e7ceee962d76b774191b98dede7dc1296a89
Akshat Saxena <saxenaakshat16@gmail.com>
2025-07-14 23:45:00 +0000

    Created presentation level transactions model

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -13,24 +13,24 @@ stg_postgres__customers as (
 , final as (
     select
         -- ids
-        stg_postgres__customers.customer_id
+        int_customers__segmented.customer_id
 
         -- strings
         , stg_postgres__customers.first_name
         , stg_postgres__customers.last_name
         , stg_postgres__customers.email
-        , stg_postgres__customers.country_code
+        , stg_postgres__customers.customer_country_code
 
         -- numerics
-        , stg_postgres__customers.age
+        , stg_postgres__customers.customer_age
 
         -- dates
-        , stg_postgres__customers.signup_date
+        , stg_postgres__customers.customer_signup_date
         , int_customers__segmented.first_transaction_date
         , int_customers__segmented.last_transaction_date
 
         -- metrics
-        , int_customers__segmented.months_active
+        , int_customers__segmented.months_active_as_customer
         , int_customers__segmented.total_spending
         , int_customers__segmented.total_transactions
         , int_customers__segmented.total_unique_products_ordered
@@ -39,13 +39,13 @@ stg_postgres__customers as (
         , int_customers__segmented.average_number_of_new_products_ordered_per_month
 
         -- segments
-        , int_customers__segmented.spending_category
-        , int_customers__segmented.purchase_frequency_category
-        , int_customers__segmented.product_diversity_category
+        , int_customers__segmented.customer_spending_category
+        , int_customers__segmented.customer_purchase_frequency_category
+        , int_customers__segmented.customer_product_diversity_category
 
-    from stg_postgres__customers
-    left join int_customers__segmented
-        on stg_postgres__customers.customer_id = int_customers__segmented.customer_id
+    from int_customers__segmented
+    left join stg_postgres__customers
+        on int_customers__segmented.customer_id = stg_postgres__customers.customer_id
 )
 
 select *
